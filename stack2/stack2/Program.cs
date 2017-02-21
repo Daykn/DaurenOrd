ee5fdf4e446f554f229f23da571c265bec4f2dab
Daykn <Daykn@users.noreply.github.com>
2017-02-22 00:33:37 +0600

    mystack

---
+++ stack2/stack2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace stack2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"1papka";
+            Stack<DirectoryInfo> dir = new Stack<DirectoryInfo>();
+            dir.Push(new DirectoryInfo(path));
+
+            while (dir.Count != 0) {
+                DirectoryInfo dil = dir.Pop();
+                foreach (DirectoryInfo di in dil.GetDirectories()) {
+                    Console.WriteLine(di.Name);
+                    dir.Push(di);
+                }
+                foreach (FileInfo fi in dil.GetFiles())
+                {
+                    Console.WriteLine(fi.Name);
+                    
+                }
+                
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
