d0af08d7c93bf3ac1c6ef6ee92beff75e5b678e7
Daykn <Daykn@users.noreply.github.com>
2017-04-27 13:42:16 +0600

    just driving car

---
+++ 3/3/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Graphics gb;
+        Mashinka car;
+        Bitmap bmp;
+        public int dx = 5;
+        public int x = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = pictureBox1.CreateGraphics();
+            gb = Graphics.FromImage(bmp);
+            timer1.Start();
+        }
+        public void draw()
+        {
+            
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+            car = new Mashinka(x, 50);
+            car.mdrow(g, new SolidBrush(Color.Red), new SolidBrush(Color.Black));
+            x += dx;
+            
+        }
+    }
+}
---
+++ 3/3/Mashinka.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace _3
+{
+    class Mashinka
+    {
+        public int x, y;
+
+        public Mashinka(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+            
+        }
+        public void mdrow(Graphics g, Brush b,Brush c)
+        {
+            g.FillRectangle(b, x + 50, y, 50, 100);
+            g.FillRectangle(b, x, y + 50, 100, 50);
+            g.FillEllipse(c, x + 35, y + 80, 30, 30);
+            g.FillEllipse(c, x + 70, y + 80, 30, 30);
+        }
+    }
+    
+}
